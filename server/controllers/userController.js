6daeedf5166944ae1bb207f5162b39f223e4c68e
Minakshi <jhaminakshi333@gmail.com>
2025-07-01 08:17:57 +0545

    Added backend server code

---
+++ server/controllers/userController.js
@@ -0,0 +1,38 @@
+
+// Signup a new user
+
+import { generateToken } from "../lib/utils";
+import User from "../models/User";
+
+export const signup = async(req,res)=>{
+    const {fullName, email, password, bio} = req.body;
+    try {
+       if (!fullName || !email || !password || !bio) {
+        return res.json({success:false, message: 'Missing Details'})
+       }
+       const user = await User.findOne({email});
+
+       if(user){
+        return res.json({ success: false, message: "Account Already Exists" });
+       }
+
+       const salt = await bcrypt.genSalt(10);
+       const hashedPass = await bcrypt.hash(password,salt);
+
+       const newUser = await User.create({
+        fullName,email, password: hashedPass, bio
+       });
+
+       const token = generateToken(newUser._id)
+       res.json({success:true, userData:newUser,
+        token, message:'Account created successfully'
+       })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({
+          success: false,
+          message:error.message
+        });
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import  'dotenv/config';
+import cors from 'cors';
+import http from 'http';
+import { connectDB } from "./lib/db.js";
+
+// Create Express app and HTTP server
+const app = express();
+const server = http.createServer(app)
+
+// Middleware Setup
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status",(req,res)=>
+res.send("Server is live") );
+
+// Connect to MongoDB
+await connectDB();
+
+const PORT = process.env.PORT || 5001;
+server.listen(PORT,()=>{
+console.log("Server is running on Port: " + PORT);
+}).on("error", (err) => {
+  if (err.code === "EADDRINUSE") {
+    console.error(`Port ${PORT} is already in use`);
+    process.exit(1);
+  } else {
+    console.error(err);
+  }
+});
