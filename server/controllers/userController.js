2c4bff3e3bd1af06247c9f5e1fed3f06c18cae07
Minakshi Kumari <jhaminakshi333@gmail.com>
2025-07-02 17:22:17 +0545

    Updated server files: auth middleware, user controller, and added cloudinary & routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 
 // Signup a new user
 
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils";
 import User from "../models/User";
 
@@ -62,4 +63,39 @@ export const login = async (req,res) => {
           message:error.message
         });
   } 
+}
+
+// Cntroller to check if user is authenticated
+export const checkAuth = async (req,res) => {
+  res.json({
+          success: true,
+          user:req.user
+        });
+}
+
+// Cntroller to update user profile details
+export const updateProfile = async (req,res) => {
+  try {
+    const {profilePic, bio, fullName} = req.body;
+    const userId = req.User._id ;
+    let updatedUser;
+
+    if(!profilePic){
+      await User.findByIdAndUpdate(userId , {bio,fullName},{new:true});
+    }else{
+      const upload = await cloudinary.uploader.upload(profilePic);
+      updatedUser = await User.findByIdAndUpdate(userId ,
+         {profilePic: upload.secure_url ,bio,fullName},{new:true});
+    }
+    res.json({
+          success: true,
+          user:updatedUser
+        });
+  } catch (error) {
+    console.log(error.message)
+        res.json({
+          success: false,
+          message:error.message
+        });
+  }
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,23 @@
 
 // Middleware to protect routes
 
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
 export const protectRoute = async (req, res, next) => {
     try {
+        const token = req.headers.token;
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        const user = await User.findById().select("-password");
+        if(!user){
+          return res.json({
+          success: false,
+          message:"User not found"
+        });
+        }
+        req.user = user;
+        next();
         
     } catch (error) {
         console.log(error.message)
